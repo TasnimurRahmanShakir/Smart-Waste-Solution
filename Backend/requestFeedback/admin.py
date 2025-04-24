33e815aa59b2a61a5a764701aeadad0cbf256a93
TasnimurRahmanShakir <tasnim.bd.cse@gmail.com>
2025-04-24 09:39:51 +0600

    remove unnessesary files such as __pycache__, env files

--- Backend/requestFeedback/admin.py
+++ Backend/requestFeedback/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import RequestFeedback
 # Register your models here.
+
+@admin.register(RequestFeedback)
+class RequestFeedbackAdmin(admin.ModelAdmin):
+    list_display = ('id', 'requested_by', 'requested_bin', 'message', 'status', 'created_at', 'request_type')
+    search_fields = ('user__email', 'requested_by')
+    list_filter = ('created_at',)
+    ordering = ('-created_at',)
